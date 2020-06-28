d7127019d441c9b2c50508fee31d2a678db0b088
valorad <valorad@outlook.com>
2020-06-28 17:03:18 -0400

    handle esc key on app

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,45 @@ import HTTP404View from "./views/HTTP404";
 import { selectTitle, setTitle } from "./features/navbar";
 import useHashLocation from "./router/useHashLocation";
 
+const placeRoutingTable = () => {
+  return (
+    <Switch>
+      <Route path="/">
+        <Redirect to="/index" />
+      </Route>
+      <Route path="/index" component={HomeView}></Route>
+      {/* <Route path="/users" component={UserListView}></Route>
+      <Route path="/users/:dbname" component={UserDetailView}></Route> */}
+      <Route path="/:rest*" component={HTTP404View}></Route>
+    </Switch>
+  );
+};
 
 export default () => {
 
   const title = useSelector(selectTitle);
 
+  const onESCKeyDown = (e: React.KeyboardEvent) => {
+    // e.preventDefault();
+
+    switch (e.key) {
+      case "GoBack":
+      case "Escape":
+      case "Cancel":
+      case "Tab":
+        window.history.back();
+        break;
+      default:
+        break;
+    }
+  }
+
+
   return (
 
     <Router hook={useHashLocation}>
 
-      <section className="App">
+      <section className="App" tabIndex={0} onKeyDown={onESCKeyDown}>
         <div className="overlay"></div>
         <div className="scanline"></div>
         <div className="wrapper">
@@ -33,15 +62,7 @@ export default () => {
             <main>
               <div className="contentBox">
 
-                <Switch>
-                  <Route path="/">
-                    <Redirect to="/index" />
-                  </Route>
-                  <Route path="/index" component={HomeView}></Route>
-                  {/* <Route path="/users" component={UserListView}></Route>
-                  <Route path="/users/:dbname" component={UserDetailView}></Route> */}
-                  <Route path="/:rest*" component={HTTP404View}></Route>
-                </Switch>
+                {placeRoutingTable()}
 
               </div>
             </main>
