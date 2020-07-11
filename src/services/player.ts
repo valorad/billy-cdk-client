291075b29d0d17489e3de3d0aa6f1b637fcac29c
valorad <valorad@outlook.com>
2020-07-11 11:02:37 -0400

    graphql player detail

--- src/services/player.ts
+++ src/services/player.ts
@@ -5,47 +5,66 @@ interface FieldSwitch {
   [index: string]: boolean
 }
 
-export const usePlayerList = (conditions: any = {}, fieldSwitch: FieldSwitch = {}, options: any = {}) => {
+const fieldSwitch0: FieldSwitch = {
+  id: true,
+  dbname: true,
+  name: true,
+  bio: false,
+  isPremium: true,
+  games: false,
+}
 
+const buildIncludedFields = (fieldSwitch: FieldSwitch) => {
+  const includedFields: string[] = [];
   const mainFieldSwitch: FieldSwitch = {
-    id: true,
-    dbname: true,
-    name: true,
-    bio: true,
-    isPremium: true,
-    games: true,
+    ...fieldSwitch0,
     ...fieldSwitch,
   }
-
-  const fieldToken: string[] = [];
-
   for (let key in mainFieldSwitch) {
     if (mainFieldSwitch[key]) {
-      fieldToken.push(key);
+      includedFields.push(key);
     }
   }
+  return includedFields;
+};
+
+export const usePlayerList = (conditions: any = {}, fieldSwitch: FieldSwitch = {}, options: any = {}) => {
+
+  // const mainFieldSwitch: FieldSwitch = {
+  //   ...fieldSwitch0,
+  //   ...fieldSwitch,
+  // }
+
+  // const fieldToken: string[] = [];
+
+  // for (let key in mainFieldSwitch) {
+  //   if (mainFieldSwitch[key]) {
+  //     fieldToken.push(key);
+  //   }
+  // }
+
+  const includedFields = buildIncludedFields(fieldSwitch);
 
   let queryToken = `
     query getPlayers($condition: String, $options: String) {
       players: players(condition: $condition, options: $options) {
-        ${fieldToken.join(",")}
+        ${includedFields.join(",")}
       }
     }
   `;
 
-  queryToken.replace(/\s/, "");
+  queryToken = queryToken.replace(/\s/, "");
 
   const queryVariables: any = {
     condition: conditions,
     options: {
-      includes: fieldToken,
+      includes: includedFields,
       ...options
     },
   };
 
-  for (let key in queryVariables) {
-    JSON.stringify(queryVariables[key]);
-  }
+  queryVariables.condition = JSON.stringify(queryVariables.condition);
+  queryVariables.options = JSON.stringify(queryVariables.options);
 
   const { loading: isQueryLoading, error: queryError, data } = useQuery<{players: Player[]}>(
     gql(queryToken),
@@ -65,5 +84,49 @@ export const usePlayerList = (conditions: any = {}, fieldSwitch: FieldSwitch = {
     queryError,
     players,
   }
+
+};
+
+export const usePlayerDetail = (dbname: string, fieldSwitch: FieldSwitch = {}, options: any = {}) => {
+  const includedFields = buildIncludedFields(fieldSwitch);
+  let queryToken = `
+    query getPlayer($dbname: String!, $options: String) {
+      player: player(dbname: $dbname, options: $options) {
+        ${includedFields.join(",")}
+      }
+    }
+  `;
+
+  queryToken = queryToken.replace(/\s/, "");
+
+  const queryVariables: any = {
+    dbname: dbname,
+    options: {
+      includes: includedFields,
+      ...options
+    },
+  };
+
+  queryVariables.options = JSON.stringify(queryVariables.options);
+
+  const { loading: isQueryLoading, error: queryError, data } = useQuery<{player: Player}>(
+    gql(queryToken),
+    {
+      variables: queryVariables
+    }
+  );
+
+  if (queryError) {
+    console.error(queryError);
+  }
+
+  const player = (data? data.player: undefined);
+
+  return {
+    isQueryLoading,
+    queryError,
+    player,
+  }
+
 
 };
--- src/views/player/PlayerDetail.tsx
+++ src/views/player/PlayerDetail.tsx
@@ -11,6 +11,7 @@ import { Player } from "../../models/player.interface";
 import PlayerDetail from "../../components/player/detail";
 import DialogConfirmation from "../../components/modal/dialogConfirmation";
 import DialogInput from "../../components/modal/dialogInput";
+import { usePlayerDetail } from "../../services/player";
 
 // import "./Dummy.scss";
 
@@ -21,14 +22,16 @@ export default () => {
   let dbname = params?.dbname || "mr-stranger";
 
   const dispatch = useDispatch();
+  const { isQueryLoading, queryError, player } = usePlayerDetail(dbname, {bio: true});
 
-  const player: Player = {
-    dbname: dbname,
-    name: "Angular Biter",
-    bio: "Angular biter's Ding-ding gas station: Zu zu zu zu zu!",
-    isPremium: true,
-    games: [],
-  };
+  // test data
+  // const player: Player = {
+  //   dbname: dbname,
+  //   name: "Angular Biter",
+  //   bio: "Angular biter's Ding-ding gas station: Zu zu zu zu zu!",
+  //   isPremium: true,
+  //   games: [],
+  // };
 
   const [editDialogResult, setEditDialogResult] = useState({});
   const editPlayer = () => {
@@ -61,50 +64,118 @@ export default () => {
   useEffect(() => {
     
     dispatch(setTitle("玩家详情"));
-    dispatch(setDescription(player.name || player.dbname));
+    dispatch(setDescription(player?.name || player?.dbname || "神秘的陌生人"));
     
   });
 
+  // during test
+  // const placePlayerList = () => {
+  //   return (
+        // <section className="PlayerDetail">
+        //   <PlayerDetail player={player} menus={menus} />
 
-  return (
-    <section className="PlayerDetail">
-      <PlayerDetail player={player} menus={menus} />
+        //   <DialogInput
+        //     dialogID="dialogInput-editPlayer"
+        //     title={`编辑信息:${player.dbname}`}
+        //     description="请填写以下信息"
+        //     items={[
+        //       {
+        //         propName: "name",
+        //         name: "玩家名称",
+        //         value: player.name || player.dbname,
+        //         isRequired: true,
+        //       },
+        //       {
+        //         propName: "isPremium",
+        //         name: "是黄金高端土豪会员",
+        //         value: player.isPremium,
+        //         type: "checkBox",
+        //       },
+        //     ]}
+        //     onFinish={(data: any) => {
+        //       setEditDialogResult({...editDialogResult, data});
+        //       console.log(data);
+        //     }}
+        //   />
 
-      <DialogInput
-        dialogID="dialogInput-editPlayer"
-        title={`编辑信息:${player.dbname}`}
-        description="请填写以下信息"
-        items={[
-          {
-            propName: "name",
-            name: "玩家名称",
-            value: player.name || player.dbname,
-            isRequired: true,
-          },
-          {
-            propName: "isPremium",
-            name: "是黄金高端土豪会员",
-            value: player.isPremium,
-            type: "checkBox",
-          },
-        ]}
-        onFinish={(data: any) => {
-          setEditDialogResult({...editDialogResult, data});
-          console.log(data);
-        }}
-      />
+        //   <DialogConfirmation
+        //     dialogID="dialogConfirmation-deletePlayer"
+        //     title={`删除玩家:${player.dbname}`}
+        //     description={"警告！此操作无法回滚！"}
+        //     onFinish={() => {
+        //       console.log(`Delete boom boom boom`);
+        //       MicroModal.close("dialogConfirmation-deletePlayer");
+        //     }}
+        //   />
 
-      <DialogConfirmation
-        dialogID="dialogConfirmation-deletePlayer"
-        title={`删除玩家:${player.dbname}`}
-        description={"警告！此操作无法回滚！"}
-        onFinish={() => {
-          console.log(`Delete boom boom boom`);
-          MicroModal.close("dialogConfirmation-deletePlayer");
-        }}
-      />
+        // </section>
+  //   );
+  // };
 
+  const placePlayerDetail = () => {
 
-    </section>
-  );
+    if (isQueryLoading) {
+      return (
+        <div className="statusInfo">
+          <h1>载入中，请稍后...</h1>
+        </div>
+      );
+    } else if (queryError || player === undefined) {
+      return (
+        <div className="statusInfo">
+          <h1>错误！无法进行玩家查询。请联系管理员。</h1>
+        </div>
+      );
+    } else if (player === null) {
+      return (
+        <div className="statusInfo">
+          <h1>错误！未找到该玩家。请联系管理员。</h1>
+        </div>
+      );
+    } else {
+      return (
+        <section className="PlayerDetail">
+          <PlayerDetail player={player} menus={menus} />
+
+          <DialogInput
+            dialogID="dialogInput-editPlayer"
+            title={`编辑信息:${player.dbname}`}
+            description="请填写以下信息"
+            items={[
+              {
+                propName: "name",
+                name: "玩家名称",
+                value: player.name || player.dbname,
+                isRequired: true,
+              },
+              {
+                propName: "isPremium",
+                name: "是黄金高端土豪会员",
+                value: player.isPremium,
+                type: "checkBox",
+              },
+            ]}
+            onFinish={(data: any) => {
+              setEditDialogResult({...editDialogResult, data});
+              console.log(data);
+            }}
+          />
+
+          <DialogConfirmation
+            dialogID="dialogConfirmation-deletePlayer"
+            title={`删除玩家:${player.dbname}`}
+            description={"警告！此操作无法回滚！"}
+            onFinish={() => {
+              console.log(`Delete boom boom boom`);
+              MicroModal.close("dialogConfirmation-deletePlayer");
+            }}
+          />
+
+        </section>
+      );
+    }
+
+  };
+
+  return placePlayerDetail();
 };
