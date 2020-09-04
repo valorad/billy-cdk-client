832080fe452895dd36ad9431f11411d1e9c19f12
valorad <valorad@outlook.com>
2020-09-04 14:24:21 -0400

    production mode graphql endpoint

--- src/app/graph.ts
+++ src/app/graph.ts
@@ -1,9 +1,11 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 
+const isProdMode = (process.env.NODE_ENV.toLowerCase() === "production");
+
 export default new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
-    uri: 'http://localhost:5000/gql',
+    uri: (isProdMode ? "/gql": "http://localhost:5000/gql"),
   }),
   defaultOptions: {
     watchQuery: {
